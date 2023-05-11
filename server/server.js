d7430face371d8f22bd4ddb884140e9850fdd46b
Xavier Teo <xvrteo@gmail.com>
2023-05-10 18:28:36 -0700

    Fixed some errors

--- server/server.js
+++ server/server.js
@@ -4,30 +4,43 @@ const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-// const routes = require('./routes');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+async function startApolloServer() {
+  const app = express();
+  const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
 
-server.applyMiddleware({ app });
+  await server.start();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  server.applyMiddleware({ app });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+  await new Promise(resolve => {
+    db.once('open', () => {
+      app.listen(PORT, () => {
+        console.log(`🌍 Now listening on localhost:${PORT}`);
+        resolve();
+      });
+    });
+  });
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+startApolloServer().catch(err => {
+  console.error(err);
+  process.exit(1);
 });
